b81ffb2169c2178c03ad45267cbed37877d34e84
Dorian1204 <lap12005@byui.edu>
2022-01-27 16:09:15 -0700

    fix
    test for the week

--- 01.25CLASS/test/login.test.js
+++ 01.25CLASS/test/login.test.js
@@ -1,10 +1,10 @@
 import assert from 'assert';
-import login from '../test/login.mjs';
+import login from '../utils/login.mjs';
 
  it("Login API should return a vailid login toke",async ()=>{
 
     const loginToken= await login({userName:"iwonttellyou@unknown.com", password: "!1Passw"});
 
-    assert.equal(loginToken.length,36);
+    assert.equal(loginToken.length,60);
 
  })
